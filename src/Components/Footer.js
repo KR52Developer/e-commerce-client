7be4cbed654b544929c1aab8358ed3cade61b756
Kavin R <127727609+KR52Developer@users.noreply.github.com>
2023-11-08 08:32:19 +0530

    final

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div style={{ textAlign: "center", padding: '20px', background: "#007bff", color: "white" }}><span >Copyright @2023.All rights reserved by trend cart. </span></div>
+        <div style={{ textAlign: "center", padding: '20px', background: "#0199cb", color: "white" }}><span >Copyright @2023.All rights reserved by trend cart. </span></div>
     );
 };
 
